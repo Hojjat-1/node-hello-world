bcacbc525636882fe3e49755805e35728df715cd
Hojjat-1 <90517184+Hojjat-1@users.noreply.github.com>
2022-01-26 13:13:02 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hiiiii');
+  res.send('Hi github');
 });
 
 app.listen(process.env.PORT || 3001, () => {
